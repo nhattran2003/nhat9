06bf8b35cbf3608f2bd95abc4e015f4a6e03aaab
nhattran2003 <126789226+nhattran2003@users.noreply.github.com>
2023-03-03 00:44:27 +0700

    Add files via upload

---
+++ 1.9.cpp
@@ -0,0 +1,87 @@
+#include<stdio.h>
+#include<conio.h>
+struct Diem
+{
+float x;
+float y;
+};
+typedef struct Diem DIEM;
+void NhapDiem(DIEM &);
+void XuatDiem(DIEM);
+struct DuongTron
+{
+DIEM l;
+
+float R;
+};
+
+typedef struct DuongTron DUONGTRON;
+
+void NhapDuongTron(DUONGTRON &);
+
+void XuatDuongTron(DUONGTRON);
+
+void NhapDiem(DIEM &d)
+
+{
+
+float temp;
+
+printf("\nNhap x: ");
+
+scanf("%f", &temp);
+
+d.x = temp;
+
+printf("\nNhap y: ");
+
+scanf("%f", &temp);
+
+d.y = temp;
+
+}
+
+void XuatDiem(DIEM d)
+
+{
+
+printf("(%8.3f, %8.3f)", d.x, d.y);
+
+}
+
+void NhapDuongTron(DUONGTRON &dt)
+{
+float temp;
+NhapDiem(dt.l);
+printf("\nNhap ban kinh: ");
+scanf("%f", &temp);
+
+dt.R = temp;
+
+}
+
+void XuatDuongTron(DUONGTRON dt)
+
+{
+
+printf("((%8.3f, %8.3f), %8.3f)", dt.l.x, dt.l.y, dt.R);
+
+}
+
+int main()
+
+{
+
+DIEM d;
+
+DUONGTRON dt;
+
+NhapDuongTron(dt);
+
+XuatDuongTron(dt);
+
+getch();
+
+return 0;
+
+}
